b95333e4ba535ff9b62b05ab1f6cde4acada5643
vickyone0 <vigneshs0.618@gmail.com>
2025-07-21 02:17:05 -0400

    config.toml

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,39 @@
-use actix_web::{App, HttpServer, web, Responder, HttpResponse};
-use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
-use env_logger;
+use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+use serde::Deserialize;
+use config::{Config, File};
+
+#[derive(Deserialize)]
+struct Settings {
+    server: ServerSettings,
+}
+
+#[derive(Deserialize)]
+struct ServerSettings {
+    host: String,
+    port: u16,
+}
 
 async fn greet() -> impl Responder {
-    HttpResponse::Ok().body("Hello, World!")
+    HttpResponse::Ok().body("hello world")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
 
-
-    //Load TLS keys
-    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
-    builder
-        .set_private_key_file("key.pem", SslFiletype::PEM)
+    let config = Config::builder()
+        .add_source(File::with_name("config"))
+        .build()
         .unwrap();
-    builder.set_certificate_chain_file("cert.pem").unwrap();
 
-    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    let settings: Settings = config.try_deserialize().unwrap();
+
+    let server_address = format!("{}:{}", settings.server.host,settings.server.port);
 
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
     })
-    .bind_openssl("127.0.0.1:8443", builder)?
-    .bind("127.0.0.1:8080")?
-    .workers(4)
+    .bind(&server_address)?
     .run()
     .await
 }
